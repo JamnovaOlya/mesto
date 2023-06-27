c77aa4420fde8aebca44edfc6c664366203c4918
Jamnova Olya <jamnova.o@yandex.ru>
2023-06-28 01:24:13 +0400

    обновить файлы

--- scripts/index.js
+++ scripts/index.js
@@ -7,29 +7,15 @@ const jobInputElement = document.querySelector('.popup__input_type_job');
 const nameEditElement = document.querySelector('.profile__title');
 const jobEditElement = document.querySelector('.profile__subtitle');
 
-console.log(editButtonElement);
-console.log(closeButtonElement);
-console.log(popupElement);
-console.log(formElement);
-console.log(nameInputElement);
-console.log(jobInputElement);
-console.log(nameEditElement);
-console.log(jobEditElement);
-
-
 function handlEditClick() {
   popupElement.classList.add('popup_opened');
   nameInputElement.value = nameEditElement.textContent;
   jobInputElement.value = jobEditElement.textContent;
 }
 
-editButtonElement.addEventListener('click', handlEditClick);
-
 function handlCloseClick() {
   popupElement.classList.remove('popup_opened');
 }
-
-closeButtonElement.addEventListener('click', handlCloseClick);
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
@@ -37,6 +23,10 @@ function handleFormSubmit(evt) {
   nameEditElement.textContent = nameInputElement.value;
   jobEditElement.textContent = jobInputElement.value;
 }
+
+editButtonElement.addEventListener('click', handlEditClick);
+
+closeButtonElement.addEventListener('click', handlCloseClick);
 
 formElement.addEventListener('submit', handleFormSubmit);
 
