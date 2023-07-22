eca1d7a55b33d19f5c95a3d835ecd24244ac179b
Jamnova Olya <jamnova.o@yandex.ru>
2023-07-22 14:49:26 +0400

    обновить файлы

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,84 @@ const nameInputElement = document.querySelector('.popup__input_type_name');
 const jobInputElement = document.querySelector('.popup__input_type_job');
 const nameEditElement = document.querySelector('.profile__title');
 const jobEditElement = document.querySelector('.profile__subtitle');
+const popupEditProfile = document.querySelector("popup_type_profile");
+const popupAddElement = document.querySelector('.popup_type_element');
+const addButtonElement = document.querySelector('.profile__add-element');
+const closeAddButtonElement = document.querySelector('.popup-add__close-button');
+const placeTitleInputElement = document.querySelector('.popup__input_type_title');
+const imageLinkInputElement = document.querySelector('.popup__input_type_image');
+const containerElement = document.querySelector('.elements');
+const formAddElement = document.querySelector('#formAddElement');
+const inputTitleAddElement = document.querySelector('#formAddElement .popup__input_type_title');
+const inputLinkAddElement = document.querySelector('#formAddElement .popup__input_type_image');
+const gridElement = document.querySelector('.elements-grid');
+const popupDeleteButtonElement = document.querySelector(".popup_type_delete");
+const cardTemplate = document.querySelector('#card').content;
 
+const initialCards = [
+  {
+    title: 'Водопады Моссбро',
+    link: 'https://images.unsplash.com/photo-1685471178855-c1fb31d4b6c3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bW9zc2JyYWUlMjBmYWxsc3xlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80'
+  },
+  {
+    title: 'Сеул',
+    link: 'https://images.unsplash.com/photo-1570192076494-f399d7681378?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fHw%3D&w=1000&q=80'
+  },
+  {
+    title: 'Гора Сечеда',
+    link: 'https://images.unsplash.com/photo-1685990678036-bbb4f85dc3dd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fHw%3D&w=1000&q=80'
+  },
+  {
+    title: 'Коала зоопарка Дуйсбурга',
+    link: 'https://images.unsplash.com/photo-1684361822885-100cc43b7f0d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fHw%3D&w=1000&q=80'
+  },
+  {
+    title: 'Динан',
+    link: 'https://images.unsplash.com/photo-1567404412779-b7316fd42107?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fHw%3D&w=1000&q=80'
+  },
+  {
+    title: 'Каппадокия',
+    link: 'https://images.unsplash.com/photo-1684863941689-2962fb53bc0e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fHw%3D&w=1000&q=80'
+  }
+];
+
+const createCard = (titleValue, linkValue) => {
+  const clone = cardTemplate.cloneNode(true);
+  const cardElement = clone.querySelector('.element');
+  const imageElement = cardElement.querySelector('.element__image');
+  const titleElement = cardElement.querySelector('.element__title');
+  const likeButtonElement = cardElement.querySelector('.element__like-button');
+  const deleteButtonElement = cardElement.querySelector('.element__delete-button');
+
+  titleElement.textContent = titleValue;
+  imageElement.src = linkValue;
+
+  imageElement.addEventListener('click', () => {
+    const cardLink = imageElement.getAttribute('src');
+    const cardTitle = titleElement.textContent;
+    openPopupImage(cardLink, cardTitle);
+  })
+
+  likeButtonElement.addEventListener('click', () => {
+    likeButtonElement.classList.toggle('element__like-button_active')
+  })
+
+  deleteButtonElement.addEventListener('click', () => {
+    const card = deleteButtonElement.parentElement;
+    card.remove()
+  })
+
+  return cardElement;
+}
+
+const handleFormAddSubmit = (evt) => {
+  evt.preventDefault();
+  const title = inputTitleAddElement.value;
+  const link = inputLinkAddElement.value;
+  const cardElement = createCard(title, link);
+  containerElement.prepend(cardElement);
+  handlCloseAddPopupClick();
+}
 
 function handlEditClick() {
   popupElement.classList.add('popup_opened');
@@ -25,121 +102,27 @@ function handleFormSubmit(evt) {
   jobEditElement.innerText = jobInputElement.value;
 }
 
+function handlAddPopupClick () {
+  popupAddElement.classList.add('popup_opened')
+}
+
+function handlCloseAddPopupClick() {
+  popupAddElement.classList.remove('popup_opened')
+}
+
+initialCards.forEach((card) => {
+  const cardElement = createCard(card.title, card.link);
+  containerElement.prepend(cardElement);
+})
+
 editButtonElement.addEventListener('click', handlEditClick);
 
 closeButtonElement.addEventListener('click', handlCloseClick);
 
 formElement.addEventListener('submit', handleFormSubmit);
 
-function closePopup(popup) {
-  popup.classList.remove("popup_opened");
-}
+addButtonElement.addEventListener('click', handlAddPopupClick)
 
-const popupCloseEditProfile = document.querySelector("popup_type_profile");
+closeAddButtonElement.addEventListener('click', handlCloseAddPopupClick)
 
-button.addEventListener('click', () => {
-  closePopup(popupCloseEditProfile);
-});
-
-
-const initialCards = [
-  {
-    name: 'Водопады Моссбро',
-    link: 'https://images.unsplash.com/photo-1685471178855-c1fb31d4b6c3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bW9zc2JyYWUlMjBmYWxsc3xlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80'
-  },
-  {
-    name: 'Сеул',
-    link: 'https://images.unsplash.com/photo-1570192076494-f399d7681378?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fHw%3D&w=1000&q=80'
-  },
-  {
-    name: 'Гора Сечеда',
-    link: 'https://images.unsplash.com/photo-1685990678036-bbb4f85dc3dd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fHw%3D&w=1000&q=80'
-  },
-  {
-    name: 'Коала зоопарка Дуйсбурга',
-    link: 'https://images.unsplash.com/photo-1684361822885-100cc43b7f0d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fHw%3D&w=1000&q=80'
-  },
-  {
-    name: 'Динан',
-    link: 'https://images.unsplash.com/photo-1567404412779-b7316fd42107?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fHw%3D&w=1000&q=80'
-  },
-  {
-    name: 'Каппадокия',
-    link: 'https://images.unsplash.com/photo-1684863941689-2962fb53bc0e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fHw%3D&w=1000&q=80'
-  }
-];
-
-const popupAddElement = document.querySelector('.popup_type_element');
-const addButtonElement = document.querySelector('.profile__add-element');
-const closeAddButtonElement = document.querySelector('.popup__close-button');
-const placeTitleInputElement = document.querySelector('.popup__input_type_title');
-const imageLinkInputElement = document.querySelector('.popup__input_type_image');
-
-
-const containerElement = document.querySelector('.elements');
-const formAddElement = document.querySelector('.popup__form');
-const inputTitleAddElement = document.querySelector('.popup__input_type_title');
-const inputLinkAddElement = document.querySelector('.popup__input_type_image');
-const gridElement = document.querySelector('.elements-grid');
-
-const template = document.querySelector('#element').content.querySelector('.element');
-
-
-const popupDeleteButtonElement = document.querySelector(".popup_type_delete");
-
-function closePopup(popup) {
-  popup.classList.remove("popup_opened");
-}
-
-popupDeleteButtonElement.addEventListener('click', () => {
-  closePopup(popupDeleteButtonElement);
-});
-
-const createCard = ({titleValue, linkValue}) => {
-  const clone = template.content.cloneNode(true);
-  const cardElement = clone.querySelector('.element');
-  const imageElement = cardElement.querySelector('.element__image');
-  const titleElement = cardElement.querySelector('.element__title');
-  const likeButtonElement = cardElement.querySelector('.element__like-button');
-  const deleteButtonElement = cardElement.querySelector('.element__delete-button');
-
-  titleElement.textContent = titleValue;
-  imageElement.src = linkValue;
-
-imageElement.addEventListener('click', () => {
-  const cardLink = imageElement.getAttribute('src');
-  const cardTitle = titleElement.textContent;
-  openPopupImage(cardLink, cardTitle);
-})
-
-likeButtonElement.addEventListener('click', () => {
-  likeButtonElement.classList.toggle('element__like-button_active')
-})
-
-deleteButtonElement.addEventListener('click', () => {
-  const card = deleteButtonElement.parentElement;
-  card.remove()
-})
-
-  return cardElement;
-}
-
-const handleFormAddSubmit = (evt) => {
-  evt.preventDefault();
-  const title = inputTitleAddElement.value;
-  const link = inputLinkAddElement.value;
-  containerElement.prepend();
-  handlClosePopupClick();
-}
-
-initialCards.forEach((card) => {
-  const cardElement = createCard(card);
-  containerElement.prepend(cardElement);
-})
-
-
-
-
-
-
-
+formAddElement.addEventListener('submit', handleFormAddSubmit);
